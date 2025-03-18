83c2198ff7e2fdde79d971151222cf705045a75c
Mike Chng <mikechngwk@gmail.com>
2025-03-18 16:10:34 +0800

    FastApi setup successful

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 
+
 app = FastAPI(title="Speech Transcription API")
+
+# Register the health check and transcribe routers
+#app.include_router(transcribe.router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Speech Transcription API!"}
+    return {"message": "FastAPI setup successful"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
